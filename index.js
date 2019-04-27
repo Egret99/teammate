20f93e3cadde925347be24ddde508072342e8294
Egret99 <Egretliu0909@163.com>
2019-04-27 19:51:12 -0400

    add google authentication

--- index.js
+++ index.js
@@ -1,8 +1,50 @@
 const app = require('express')();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+// const {
+//     googleClientID,
+//     googleClientSecret
+// } = require('./config/keys');
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send({msg: "Hello"});
+passport.use(
+    new GoogleStrategy({
+        clientID: process.env.googleClientID,
+        clientSecret: process.env.googleClientSecret,
+        callbackURL: '/return'
+    }, (accessToken, refreshToken, profile, cb) => {
+        return cb(null, profile)
+    })
+);
+
+passport.serializeUser(function (user, cb) {
+    cb(null, user);
+});
+
+passport.deserializeUser(function (obj, cb) {
+    cb(null, obj);
+});
+
+app.use(passport.initialize());
+
+app.get('/login/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+app.get('/return', passport.authenticate('google'), (req, res) => {
+    res.send({
+        id: req.user.id,
+        name: req.user.displayName,
+        email: req.user.emails[0].value
+    });
+});
+
+app.get('/me', (req, res) => {
+    console.log(req.userProfile);
+    res.send({
+        user: req.session.user
+    });
 })
 
 app.listen(PORT, () => {
